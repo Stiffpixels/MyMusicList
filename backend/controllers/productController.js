55c25f7874d76f4a6c479723705bcbe3dcb4637a
Muzammil <saifimuzammil@@gmail.com>
2023-05-19 14:21:51 +0530

    Adding more depth to filtering system and update, delete operation

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,8 +3,10 @@ const ErrorHandler = require('../utils/errorHandler')
 
 
 const getProducts = async (req, res, next)=>{
-    const { name, price, fields, category } = req.query;
+    const { name, price, fields, category, numFilters } = req.query;
     const qParams = {}
+
+    
 
     if(name){
         qParams.name = { $regex:name, $options:'i' }
@@ -12,6 +14,27 @@ const getProducts = async (req, res, next)=>{
 
     if(category){
         qParams.category ={ $in: category.split(',') }
+    }
+    
+    if(numFilters){
+        const operatorMap ={
+            ">":"$gt",
+            ">=":"$gte",
+            "==":"$eq",
+            "<":"$lt",
+            "<=":"$lte"
+        }
+
+        const re = /\b(>|>=|==|<+|<)\b/g
+
+        const mongoNumFilters = numFilters.replace(re, (match)=>`-${operatorMap[match]}-`)
+        
+        mongoNumFilters.split(",").map((filter)=>{
+            
+            const [ field, operator, value ] = filter.split('-')
+            qParams[field] = { [operator]:Number(value) }
+        })
+        
     }
 
     let results = Product.find(qParams)
@@ -60,7 +83,35 @@ const addProduct = async (req, res)=>{
 }
 
 const updateProduct = async (req, res)=>{
-    const status = await Product.findOneAndUpdate({_id:req.query.id}, req.body, { new:true })
+    const { id, field } = req.query
+    let status;
+    const newValues = req.body
+    const fieldQuery = {}
+
+    if(id){
+         status = await Product.findOneAndUpdate({_id:req.query.id}, newValues, { new:true })
+    }
+    if(field){
+        const fieldList = field.split(",")
+
+        switch(fieldList[0]){
+            case 'name':
+                fieldQuery.name = fieldList[1]
+                break;
+            case 'price':
+                fieldQuery.price = fieldList[1]
+                break;
+            case 'category':
+                fieldQuery.category = fieldList[1]
+                break;
+            case 'rating':
+                fieldQuery.rating = fieldList[1]
+        }
+        
+        console.log(fieldQuery)
+        status = await Product.updateMany( fieldQuery, {$set:newValues},{ multi:true }) 
+    }
+    
 
     res.status(200).json({
         success:true,
@@ -78,8 +129,25 @@ const deleteProducts = async (req, res)=>{
     if(field){
         const fieldList = field.split(",")
         const fieldQuery = {}
-        fieldQuery[fieldList[0]] = fieldList[1]
 
+        switch(fieldList[0]){
+            case 'name':
+                if(fieldList[1]==='notexists'){
+                    fieldQuery.name = { $exists:false }
+                }else{
+                    fieldQuery.name = fieldList[1]
+                }
+                break;
+            case 'price':
+                fieldQuery.price = fieldList[1]
+                break;
+            case 'category':
+                fieldQuery.category = fieldList[1]
+                break;
+            case 'rating':
+                fieldQuery.rating = Number(fieldList[1])
+        }
+        
         product.products = await Product.deleteMany(fieldQuery)
     }
     res.status(200).json({
