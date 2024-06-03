3dfe9e9b7e168faa60e00b87ab00be1f3d3e89db
unknown <saifimuzammil2@gmail.com>
2024-06-03 16:34:44 +0530

    vercel api deployment config

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,29 @@
-const express = require("express")
-const app = express()
-require('express-async-errors')
-const errorMiddleware = require("./middleware/errMiddleware")
-const cookieParser = require('cookie-parser')
-const cors  = require('cors')
-const path = require('path')
-
+const express = require("express");
+const app = express();
+require("express-async-errors");
+const errorMiddleware = require("./middleware/errMiddleware");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const path = require("path");
 
 //route imports
-const musicRouter = require("./routes/musicRoutes")
-const userRouter = require("./routes/userRoutes")
+const musicRouter = require("./routes/musicRoutes");
+const userRouter = require("./routes/userRoutes");
 
 //middleware
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({credentials:true, origin:['http://localhost:3000', "https://mymusiclist-muzammil.netlify.app"]}))
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ credentials: true, origin: ["http://localhost:3000", "https://mymusiclist-muzammil.netlify.app"] }));
 
 //routes
-app.use("/api/v1", musicRouter)
-app.use("/api/v1", userRouter)
+app.use("/api/v1", musicRouter);
+app.use("/api/v1", userRouter);
 
 //error handler middleware
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
-app.get('*', (req, res)=>{
-    res.send("couldn't get" + req.path)
-})
+app.get("*", (req, res) => {
+  res.send("couldn't get" + req.path);
+});
 
-module.exports = app
+export default app;
